dccb5a42f32e3d96eadfb8fe1dd343a647c57329
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2021-11-16 17:05:27 +0800

    update: random-machine.html device hand controller

--- src/random-machine.js
+++ src/random-machine.js
@@ -2,6 +2,11 @@ import './index.css'
 let getRandomBtn = document.querySelector('#getRandom');
 let randomNumber = document.querySelectorAll('.randomNumber');
 
+const randomNumberUI = document.querySelector('#randomNumberUI');
+const deviceHandController = document.querySelector('#deviceHandController');
+const leftHandDevice = document.querySelector('#leftHandDevice');
+const rightHandDevice = document.querySelector('#rightHandDevice');
+
 getRandomBtn.addEventListener('click', function() {
     let randomSet = [];
     randomNumber.forEach(item => {
@@ -12,4 +17,18 @@ getRandomBtn.addEventListener('click', function() {
         randomSet.push(random);
         item.innerText = random;
     });
+});
+
+leftHandDevice.addEventListener('click', function() {
+    randomNumberUI.classList.add('flex-row-reverse');
+    deviceHandController.classList.add('justify-end');
+    rightHandDevice.classList.remove('hidden');
+    leftHandDevice.classList.add('hidden');
+});
+
+rightHandDevice.addEventListener('click', function() {
+    randomNumberUI.classList.remove('flex-row-reverse');
+    deviceHandController.classList.remove('justify-end');
+    leftHandDevice.classList.remove('hidden');
+    rightHandDevice.classList.add('hidden');
 });
